49e9f06955136a7c8117c9e61a30709b53a5468b
AlexGraceW <grace2photo@gmail.com>
2025-03-18 00:41:21 -0400

    Update NotFoundException.java
    Удалил строки алгоритма git

--- src/main/java/org/example/exception/NotFoundException.java
+++ src/main/java/org/example/exception/NotFoundException.java
@@ -1,12 +1,9 @@
 package org.example.exception;
 
-<<<<<<< HEAD
-=======
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
->>>>>>> 7c81be3 (Removed added)
 public class NotFoundException extends RuntimeException {
     public NotFoundException() {
     }

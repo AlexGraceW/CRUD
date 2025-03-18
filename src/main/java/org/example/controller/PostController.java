636b525471e2d040b84b7f8eeb677ffb2bca8dc8
AlexGraceW <grace2photo@gmail.com>
2025-03-18 00:44:28 -0400

    Update PostController.java
    Удалил строки алгоритма git

--- src/main/java/org/example/controller/PostController.java
+++ src/main/java/org/example/controller/PostController.java
@@ -1,23 +1,5 @@
 package org.example.controller;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-import com.google.gson.Gson;
-import org.example.exception.NotFoundException;
-import org.example.model.Post;
-import org.example.service.PostService;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.Reader;
-
-public class PostController {
-    public static final String APPLICATION_JSON = "application/json";
-    private final PostService service;
-    private final Gson gson = new Gson();
-=======
-=======
->>>>>>> 7c81be3 (Removed added)
 import org.springframework.web.bind.annotation.*;
 import org.example.model.Post;
 import org.example.service.PostService;
@@ -28,48 +10,11 @@ import java.util.List;
 @RequestMapping("/api/posts")
 public class PostController {
     private final PostService service;
-<<<<<<< HEAD
->>>>>>> fc16aab (Ghanges added)
-=======
->>>>>>> 7c81be3 (Removed added)
 
     public PostController(PostService service) {
         this.service = service;
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public void all(HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
-        final var data = service.all();
-        response.getWriter().print(gson.toJson(data));
-    }
-
-    public void getById(long id, HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
-        try {
-            var post = service.getById(id);
-            response.getWriter().print(gson.toJson(post));
-        } catch (NotFoundException e) {
-            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-        }
-    }
-
-    public void save(Reader body, HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
-        final var post = gson.fromJson(body, Post.class);
-        final var data = service.save(post);
-        response.getWriter().print(gson.toJson(data));
-    }
-
-    public void removeById(long id, HttpServletResponse response) {
-        service.removeById(id);
-        response.setStatus(HttpServletResponse.SC_NO_CONTENT);
-    }
-}
-=======
-=======
->>>>>>> 7c81be3 (Removed added)
     @GetMapping
     public List<Post> all() {
         return service.all();
@@ -89,6 +34,7 @@ public class PostController {
     public void removeById(@PathVariable long id) {
         service.removeById(id);
     }
+}
 <<<<<<< HEAD
 }
 >>>>>>> fc16aab (Ghanges added)
